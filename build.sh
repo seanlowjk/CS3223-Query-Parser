14d20cc04317e9eafe0ad1fe27ec0885c745224d
Sean Low <sean.low.jk@gmail.com>
2021-02-16 21:05:09 +0800

    Fix Directory Structure for Application to Work

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-javac -d classes -classpath lib -sourcepath src src/QueryMain.java
-javac -d classes -classpath lib -sourcepath src src/ConvertTxtToTbl.java 
-javac -d classes -classpath lib -sourcepath src src/RandomDB.java
+javac -d $COMPONENT/classes $COMPONENT/src/qp/utils/*.java
+javac -d $COMPONENT/classes $COMPONENT/src/qp/parser/*.java 
+javac -d $COMPONENT/classes $COMPONENT/src/qp/operators/*.java 
+javac -d $COMPONENT/classes $COMPONENT/src/qp/optimizer/*.java 
+javac -d $COMPONENT/classes $COMPONENT/testcases/*.java 
+javac -d $COMPONENT/classes $COMPONENT/src/QueryMain.java 
