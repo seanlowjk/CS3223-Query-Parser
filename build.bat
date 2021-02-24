86d0a38cb628f7872549344e76be24e9999f18b4
Sean Low <sean.low.jk@gmail.com>
2021-02-24 22:11:57 +0800

    Move Testcases and update .gitignore

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-javac -d classes -classpath lib -sourcepath src src/QueryMain.java
-javac -d classes -classpath lib -sourcepath src src/ConvertTxtToTbl.java 
-javac -d classes -classpath lib -sourcepath src src/RandomDB.java 
+javac -d classes -classpath $CLASSPATH -sourcepath src src/qp/utils/*.java
+javac -d classes -classpath $CLASSPATH -sourcepath src src/qp/parser/*.java
+javac -d classes -classpath $CLASSPATH -sourcepath src src/qp/operators/*.java
+javac -d classes -classpath $CLASSPATH -sourcepath src src/qp/optimizer/*.java
+
+javac -d classes -classpath $CLASSPATH -sourcepath src src/QueryMain.java
+javac -d classes -classpath $CLASSPATH -sourcepath src src/ConvertTxtToTbl.java 
+javac -d classes -classpath $CLASSPATH -sourcepath src src/RandomDB.java
