3826693f332fcf7e3c2c23b50fb1d57c06903db1
Sean Low <sean.low.jk@gmail.com>
2021-02-27 19:16:59 +0800

    Add Placeholders for Main Sorting Methods

--- src/qp/operators/Sort.java
+++ src/qp/operators/Sort.java
@@ -1,6 +1,8 @@
 package qp.operators;
 
 import qp.utils.AttributeDirection;
+import qp.utils.Batch;
+import qp.utils.TupleComparator;
 
 import java.util.List;
 
@@ -10,10 +12,42 @@ public class Sort extends Operator {
     private List<AttributeDirection> attributeDirections;
     private int numberOfBuffers;
 
+    private TupleComparator comparator;
+    private int roundNumber;
+    private int fileNumber;
+
     public Sort(Operator op, List<AttributeDirection> attributeDirections, int numberOfBuffers, int type) {
         super(type);
         this.op = op;
         this.attributeDirections = attributeDirections;
         this.numberOfBuffers = numberOfBuffers;
+
+        this.comparator = new TupleComparator(op.getSchema(), this.attributeDirections);
+        this.roundNumber = 0;
+        this.fileNumber = 0;
+    }
+
+    @Override
+    public boolean open() {
+        int tuplesize = schema.getTupleSize();
+        int batchsize = Batch.getPageSize() / tuplesize;
+
+        createSortedRuns();
+        mergeSortedRuns();
+
+        return true;
+    }
+
+    private void createSortedRuns() {
+
+    }
+
+    private void mergeSortedRuns() {
+
+    }
+
+    private void gotoNextRound() {
+        this.roundNumber ++;
+        this.fileNumber = 0;
     }
 }
