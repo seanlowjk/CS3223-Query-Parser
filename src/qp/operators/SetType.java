83c6ce44333d0021f9af8a69416efd28a212acae
Sean Low <sean.low.jk@gmail.com>
2021-03-13 16:51:30 +0800

    Add Types for Set Operations

---
+++ src/qp/operators/SetType.java
@@ -0,0 +1,13 @@
+/**
+ * Enumeration of set operation types
+ * Change this class depending on actual set operations
+ * you have implemented in your query processor
+ **/
+
+package qp.operators;
+
+public class JoinType {
+
+    public static final int INTERSECT = 0;
+
+}
