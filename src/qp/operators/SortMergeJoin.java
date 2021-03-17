9651e826470a1b31f6eb9dd4b333ae4564b25b07
Sean Low <sean.low.jk@gmail.com>
2021-03-17 15:20:50 +0800

    Final Fixes for backtracking Algorithm

--- src/qp/operators/SortMergeJoin.java
+++ src/qp/operators/SortMergeJoin.java
@@ -238,8 +238,7 @@ public class SortMergeJoin extends Join {
                         outbatch.add(outtuple);
                         bcurs++;
                     } else {
-                        readNextLeftTuple();
-                        rcurs ++;
+                        // System.out.printf("Poof: %s %s\n", leftbatch.get(lcurs)._data, rightbatch.get(rcurs)._data);
                         isBacktracking = false;
                         eosb = true;
                         sosb = false; 
@@ -254,7 +253,7 @@ public class SortMergeJoin extends Join {
                 System.out.println("SortMergeJoin: Error in reading temporary file");
             }
             eosb = true;
-            System.out.printf("End: %s", leftbatch.get(lcurs)._data);
+            // System.out.printf("End: %s", leftbatch.get(lcurs)._data);
         } catch (ClassNotFoundException c) {
             System.out.println("SortMergeJoin: Error in deserialising temporary file ");
             System.exit(1);
@@ -283,6 +282,9 @@ public class SortMergeJoin extends Join {
                         outbatch.add(outtuple);
                         rcurs++;
 
+                        // System.out.printf("Try: %s %s\n", lefttuple._data, righttuple._data);
+                        // System.out.printf("Try: %s %s\n", lefttuple._data, rightbatch.get(rcurs)._data);
+
                         // If there is a condition to backtrack, initialize the backtracking file 
                         if (prevtuple == null || !prevtuple.checkJoin(lefttuple, leftindex, leftindex)) {
                             prevtuple = lefttuple;
@@ -306,14 +308,16 @@ public class SortMergeJoin extends Join {
                         if (comparisonfactor > 0) {
                             rcurs++;
                         } else { // comparisonfactor < 0;
-                            readNextLeftTuple();
                             if (isBacktracking) {
                                 bout.writeObject(outbackbatch);
                                 outbackbatch = new Batch(batchsize);
                                 bout.close(); 
+                                // System.out.printf("Go: %s %s\n", lefttuple._data, rightbatch.get(rcurs)._data);
                                 eosb = true; 
                                 sosb = true;
                                 return; 
+                            } else {
+                                readNextLeftTuple();
                             }
 
                             if (lcurs >= leftbatch.size()) {
