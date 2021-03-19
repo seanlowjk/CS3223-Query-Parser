749840ffb2ec51ae21a1f1f6726a65950ebc92cb
Low Jun Kai, Sean <42912708+seanlowjk@users.noreply.github.com>
2021-03-19 16:58:25 +0800

    Remove INTERSECT ALL  (#54)
    * Fix Intersect
    
    * Fix'

--- src/qp/operators/Intersect.java
+++ src/qp/operators/Intersect.java
@@ -1,11 +1,9 @@
-/**
- * This is base class for the INTERSECT operator
- **/
-
 package qp.operators;
 
+import qp.utils.Attribute;
 import qp.optimizer.BufferManager;
 import qp.utils.Batch;
+import qp.utils.Condition;
 import qp.utils.Tuple;
 
 import java.io.EOFException;
@@ -16,33 +14,35 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
- * The type Intersect.
+ * Block Nested Join operator.
  */
 public class Intersect extends SetOperator {
 
     static int filenum = 0;         // To get unique filenum for this operation
     int batchsize;                  // Number of tuples per out batch
-    ArrayList<Integer> leftindex;   // Indices of the set attributes in left table
+    ArrayList<Integer> leftindex;   // Indices of the join attributes in left table
+    ArrayList<Integer> rightindex;  // Indices of the join attributes in right table
     String rfname;                  // The file name where the right table is materialized
     Batch outbatch;                 // Buffer page for output
-    Queue<Tuple> leftBlock;         // Buffer block for left input stream
+    ArrayList<Batch> leftBlock;     // Buffer block for left input stream
+    Batch leftbatch;                // Buffer page for left input stream
     Batch rightbatch;               // Buffer page for right input stream
     ObjectInputStream in;           // File pointer to the right hand materialized file
 
+    int lbatch;                     // Cursor for left side buffer for batch number
+    int lcurs;                      // Cursor for left side buffer for tuple number
     int rcurs;                      // Cursor for right side buffer
     boolean eosl;                   // Whether end of stream (left table) is reached
     boolean eosr;                   // Whether end of stream (right table) is reached
 
     /**
-     * Instantiates a new Intersect.
+     * Instantiates a new Block nested join.
      *
-     * @param left   the left
-     * @param right  the right
-     * @param opType the op type
+     * @param jn generic Join operator that
+     *           carries information required
+     *           for joining.
      */
     public Intersect(Operator left, Operator right, int opType) {
         super(left, right, opType);
@@ -51,9 +51,9 @@ public class Intersect extends SetOperator {
     }
 
     /**
-     * During open finds the index of the set attributes.
+     * During open finds the index of the join attributes
      * * Materializes the right hand side into a file
-     * * Opens the connections,
+     * * Opens the connections
      **/
     @Override
     public boolean open() {
@@ -61,14 +61,18 @@ public class Intersect extends SetOperator {
         int tuplesize = schema.getTupleSize();
         batchsize = Batch.getPageSize() / tuplesize;
 
-        /** find indices attributes of equality conditions **/
+        /** find indices attributes of join conditions **/
         leftindex = new ArrayList<>();
+        rightindex = new ArrayList<>();
         for (int i = 0; i < left.getSchema().getAttList().size(); i++) {
             leftindex.add(i);
+            rightindex.add(i);
         }
         Batch rightpage;
 
         /** initialize the cursors of input buffers **/
+        lbatch = 0;
+        lcurs = 0;
         rcurs = 0;
         eosl = false;
         /** because right stream is to be repetitively scanned
@@ -77,10 +81,10 @@ public class Intersect extends SetOperator {
         eosr = true;
 
         /* Initialize the leftBlock, aka the left buffer. */
-        leftBlock = new LinkedList<>();
+        leftBlock = new ArrayList<>();
 
         /** Right hand side table is to be materialized
-         ** for the Intersection to perform
+         ** for the  Block Nested join to perform
          **/
         if (!right.open()) {
             return false;
@@ -90,7 +94,7 @@ public class Intersect extends SetOperator {
              ** into a file
              **/
             filenum++;
-            rfname = "Itemp-" + String.valueOf(filenum);
+            rfname = "BNJtemp-" + String.valueOf(filenum);
             try {
                 ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(rfname));
                 while ((rightpage = right.next()) != null) {
@@ -98,7 +102,7 @@ public class Intersect extends SetOperator {
                 }
                 out.close();
             } catch (IOException io) {
-                System.out.println("Intersect: Error writing to temporary file");
+                System.out.println("BlockNestedJoin: Error writing to temporary file");
                 return false;
             }
             if (!right.close())
@@ -109,145 +113,143 @@ public class Intersect extends SetOperator {
     }
 
     /**
-     * The next batch of tuples in the
-     * intersection of left and right.
-     * @return Batch containing output tuples.
-     */
+     * From input buffers selects the tuples satisfying join condition
+     * @return  a Batch object which is a page of output tuples
+     **/
     @Override
     public Batch next() {
-        outbatch = new Batch(batchsize);
-
+        int h, i, j;
         if (eosl) {
             return null;
         }
-
+        outbatch = new Batch(batchsize);
         while (!outbatch.isFull()) {
-            try {
-                // need new left page
-                if (leftBlock.isEmpty()) {
-                    /** new left page is to be fetched**/
+            if (lbatch == 0 && lcurs == 0 && eosr == true) {
+                /** new left page is to be fetched**/
+                try {
                     leftBlock = generateLeftBuffer();
-                    if (leftBlock.isEmpty()) {
-                        eosl = true;
-                        if (outbatch.isEmpty()) {
-                            return null;
-                        } else {
-                            return outbatch;
-                        }
-                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    System.exit(1);
                 }
 
-                if (eosr) {
-                    /** Whenever a new left page came, we have to start the
-                    ** scanning of right table
-                    **/
-                    try {
-                        in = new ObjectInputStream(new FileInputStream(rfname));
-                        eosr = false;
-                    } catch (IOException io) {
-                        System.err.println("Intersect:error in reading the file");
-                        System.exit(1);
-                    }
+                /**
+                 * If the new left page is empty, we have successfully
+                 * read in all the tuples from the left operator. 
+                 */
+                if (leftBlock.size() == 0) {
+                    eosl = true;
+                    return outbatch;
                 }
-            } catch (IOException io) {
-                System.err.println("Intersect:error in reading the file");
-                System.exit(1);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.exit(1);
+                /** Whenever a new left page came, we have to start the
+                 ** scanning of right table
+                 **/
+                try {
+                    in = new ObjectInputStream(new FileInputStream(rfname));
+                    eosr = false;
+                } catch (IOException io) {
+                    System.err.println("NestedJoin:error in reading the file");
+                    System.exit(1);
+                }
             }
-            // Gets the batch from the set operation. 
-            getSetBatch();
+
+            while (eosr == false) {
+                try {
+                    if (rcurs == 0 && lcurs == 0 && lbatch == 0) {
+                        rightbatch = (Batch) in.readObject();
+                    }
+                    for (h = lbatch; h < leftBlock.size(); ++h) {
+                        /**
+                         * Get the block from the left operator 
+                         */
+                        leftbatch = leftBlock.get(h);
+                        for (i = lcurs; i < leftbatch.size(); ++i) {
+                            for (j = rcurs; j < rightbatch.size(); ++j) {
+                                Tuple lefttuple = leftbatch.get(i);
+                                Tuple righttuple = rightbatch.get(j);
+                                if (Tuple.compareTuples(lefttuple, righttuple, leftindex, rightindex) == 0) {
+                                    Tuple outtuple = lefttuple.joinWith(righttuple);
+                                    outbatch.add(outtuple);
+                                    if (outbatch.isFull()) {
+                                        if (i == leftbatch.size() - 1 && j == rightbatch.size() - 1) {  //case 1
+                                            lbatch = h + 1;
+                                            lcurs = 0;
+                                            rcurs = 0;
+                                        } else if (i != leftbatch.size() - 1 && j == rightbatch.size() - 1) {  //case 2
+                                            lbatch = h;
+                                            lcurs = i + 1;
+                                            rcurs = 0;
+                                        } else if (i == leftbatch.size() - 1 && j != rightbatch.size() - 1) {  //case 3
+                                            lbatch = h;
+                                            lcurs = i;
+                                            rcurs = j + 1;
+                                        } else {
+                                            lbatch = h;
+                                            lcurs = i;
+                                            rcurs = j + 1;
+                                        }
+                                        return outbatch;
+                                    }
+                                }
+                            }
+                            rcurs = 0;
+                        }
+                        lcurs = 0;
+                    }
+                    lbatch = 0;
+                } catch (EOFException e) {
+                    try {
+                        in.close();
+                    } catch (IOException io) {
+                        System.out.println("NestedJoin: Error in reading temporary file");
+                    }
+                    eosr = true;
+                } catch (ClassNotFoundException c) {
+                    System.out.println("NestedJoin: Error in deserialising temporary file ");
+                    System.exit(1);
+                } catch (IOException io) {
+                    System.out.println("NestedJoin: Error in reading temporary file");
+                    System.exit(1);
+                }
+            }
         }
         return outbatch;
     }
 
     /**
-     * Retrieves the next batch from the Intersect Operator. 
-     * Do note that the main algorithm used here is BLock Nested Loop 
-     * Join. 
-     */
-    public void getSetBatch() {
-        try {
-            while (!eosr) {
-                if (rcurs == 0) {
-                    rightbatch = (Batch) in.readObject();
-                }
-                while (!leftBlock.isEmpty()) {
-                    Tuple leftTuple = leftBlock.peek();
-                    for (int i = rcurs; i < rightbatch.size(); i++) {
-                        Tuple rightTuple = rightbatch.get(i);
-                        if (Tuple.compareTuples(leftTuple, rightTuple, leftindex, leftindex) == 0) {
-                            outbatch.add(leftTuple);
-                            if (outbatch.isFull()) {
-                                rcurs = i;
-                                if (!leftBlock.isEmpty() && rcurs != rightbatch.size() - 1) {
-                                    rcurs++;
-                                } else if (!leftBlock.isEmpty() && rcurs == rightbatch.size() - 1) {
-                                    rightbatch = (Batch) in.readObject();
-                                    rcurs = 0;
-                                } else if (leftBlock.isEmpty() && rcurs != rightbatch.size() - 1) {
-                                    // Do Nothing here.
-                                } else { // if (leftBlock.isEmpty() && rcurs == rightbatch.size() - 1)
-                                    rightbatch = (Batch) in.readObject();
-                                    rcurs = 0;
-                                }
-                                break;
-                            }
-                        }
-                    }
-                    rightbatch = (Batch) in.readObject();
-                    rcurs = 0;
-                }
-            }
-        } catch (EOFException e) {
-            try {
-                in.close();
-            } catch (IOException io) {
-                System.out.println("Intersect: Error in reading temporary file");
-            }
-            leftBlock.poll();
-            eosr = true;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Intersect: Error in deserialising temporary file ");
-            System.exit(1);
-        } catch (IOException io) {
-            System.out.println("Intersect: Error in reading temporary file");
-            System.exit(1);
-        }
-
-    }
-
-    /**
-     * Generate left buffer block.
+     * Generate left buffer array list.
      *
-     * @return left block queue.
-     * @throws Exception the exception.
+     * @return arraylist of batches representing left buffer block.
+     * @throws Exception the exception
      */
-    public Queue<Tuple> generateLeftBuffer() throws Exception {
+    public ArrayList<Batch> generateLeftBuffer() throws Exception {
         int numAvailableBuffers = numBuff - 2;
-        Queue<Tuple> leftBuffer = new LinkedList<Tuple>();
+        ArrayList<Batch> leftBuffer = new ArrayList<>();
 
+        /**
+         * If it is the end of the left stream, return an 
+         * empty buffer. 
+         */
         if (eosl) {
             return leftBuffer;
         }
 
+        /**
+         * Fill in all the B-2 buffers when needed. 
+         */
         for (int i = 0; i < numAvailableBuffers; i++) {
             Batch leftBatch = left.next();
             if (leftBatch == null || leftBatch.isEmpty()) {
                 return leftBuffer;
             } else {
-                for(int j = 0; j < leftBatch.size(); j++) {
-                    leftBuffer.add(leftBatch.get(j));
-                }
+                leftBuffer.add(leftBatch);
             }
         }
-
         return leftBuffer;
     }
 
     /**
-     * Close the base operator.
+     * Close the operator
      */
     public boolean close() {
         File f = new File(rfname);
@@ -255,10 +257,6 @@ public class Intersect extends SetOperator {
         return left.close() && right.close();
     }
 
-    /**
-     * Clones a new Intersect Object.
-     * @return a new Intersect object.
-     */
     @Override
     public Object clone() {
         Operator newleft = (Operator) left.clone();
